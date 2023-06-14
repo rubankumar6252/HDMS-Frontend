1adb84a1ebe41774341228ab45591b3320187e14
Ruban kumar B <91937901+rubankumar6252@users.noreply.github.com>
2023-06-14 17:57:04 +0530

    Update homenav.js

--- src/components/homenav.js
+++ src/components/homenav.js
@@ -2,36 +2,54 @@ import './homenav.css';
 import Deskimg from "../components/images/helpdesk.png";
 import RvsLogo from "../components/images/rvs_logo.png";
 import { Link } from 'react-router-dom';
+import { useState } from 'react';
 
 export default function HomeNav() {
+    const [menu, setMenu] = useState(false);
+    const handleClickMenu = () => {
+        setMenu(!menu);
+    }
+
     return (
         <>
-            <nav className='navbar'>
+            <nav className={`navbar ${menu ? 'menu-open' : ''}`}>
+                <div className={`menu-container ${menu ? 'open' : ''}`}>
+                    <ul className='navbar_nav'>
+                        <div className='adminlogin'>
+                            <li className='nav_item'>
+                                <Link to="/adminlogin" className='nav_link' onClick={() => setMenu(false)}>
+                                    <span className="material-symbols-outlined">
+                                        login
+                                    </span>
+                                    <span className='admin_name'>Admin Login</span>
+                                </Link>
+                            </li>
+                        </div>
+                        <div className='userlogin'>
+                            <li className='nav_item'>
+                                <Link to="/userlogin" className='nav_link' onClick={() => setMenu(false)}>
+                                    <span className="material-symbols-outlined log">
+                                        login
+                                    </span>
+                                    <span className='userlogin_name'>User Login</span>
+                                </Link>
+                            </li>
+                        </div>
+                    </ul>
+                </div>
+                <span className={`material-symbols-outlined menu ${menu ? 'active' : ''}`} onClick={handleClickMenu}>
+                    {menu ? 'close' : 'menu'}
+                </span>
                 <div className='container'>
                     <a href='/' className='navbar_head'>
                         <img src={Deskimg} className="desk_logo" alt='navbar_header' />
                     </a>
                     <img src={RvsLogo} className="rvs_logo" alt='navbar_header' />
                     <div className='nav_rvs'>
-                    <h1 className='rvs_title'>rvs college of arts and science</h1>
+                        <h1 className='rvs_title'>rvs college of arts and science</h1>
                     </div>
-                    <ul className='navbar_nav'>
-                        <div className='adminlogin'>
-                            <li className='nav_item'><Link to="/adminlogin" className='nav_link'>
-                                <span class="material-symbols-outlined">
-                                    login
-                                </span><span className='admin_name'>Admin Login</span></Link></li>
-                        </div>
-                        <div className='userlogin'>
-                            <li className='nav_item'><Link to="/userlogin" className='nav_link'>
-                                <span class="material-symbols-outlined log">
-                                    login
-                                </span><span className='userlogin_name'>User Login</span></Link></li>
-                        </div>
-                    </ul>
                 </div>
             </nav>
-
         </>
     )
 }
